6e2fd3cc1caa6f3e02ca9a902a29dfd36fb9b35b
Daniel Jacobi <daniel@questmaster.de>
2012-07-25 02:30:09 +0200

    addedcopyright disclaimer

--- src/de/questmaster/fatremote/SelectFATActivity.java
+++ src/de/questmaster/fatremote/SelectFATActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2010 Daniel Jacobi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package de.questmaster.fatremote;
 
 import android.content.Intent;
@@ -8,7 +24,13 @@ import de.questmaster.fatremote.FatRemoteSettings.AppSettings;
 import de.questmaster.fatremote.datastructures.FATDevice;
 import de.questmaster.fatremote.fragments.SelectFATFragment;
 
-
+/**
+ * This class provides the activity shell for the SelectFATFragment. It provides the selection dialog for FAT 
+ * devices and the possibility to automatically discover devices or enter them manually.
+ * 
+ * @author daniel
+ *
+ */
 public class SelectFATActivity extends FragmentActivity implements SelectFATFragment.FATSelectedListener {
 		
 	private AppSettings mSettings = new FatRemoteSettings.AppSettings();
